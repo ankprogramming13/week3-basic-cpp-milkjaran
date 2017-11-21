57885c52bc7af88307cfab788e692921f8c00c18
Unknown <jaranyaboo@cvc-cha.ac.th>
2017-11-21 17:25:53 +0700

    ทำข้อ 1

--- lab_3_1.cpp
+++ lab_3_1.cpp
@@ -4,5 +4,13 @@ using namespace std;
 
 int main() {
   // เขียนโปรแกรมเพื่อแสดงผลตามที่กำหนด
+
+  cout << "Flullname :" << "Miss" << "Jaranya Boonmee" << endl ;
+  cout << "Nicename :" << "Milk" << endl ;
+  cout << "Gender :" << "Female" << endl;
+  cout << "Date of Birth :" << "05 january 1999" << endl ;
+  cout << "Hobby :" << "Play Games" << endl ;
+  cout << "Status :" << "single" ;
+
   return 0;
 }
