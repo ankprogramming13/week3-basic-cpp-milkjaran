145e99ef16465522698d240b8f0d1dbf57e46b63
Unknown <jaranyaboo@cvc-cha.ac.th>
2017-11-21 18:21:31 +0700

    ข้อ 3 เส็ดแล้วววววววว

--- lab_3_3.cpp
+++ lab_3_3.cpp
@@ -6,9 +6,12 @@ int main() {
 
   const float pi = 3.14315926536;
   float r;
+  float h = 15;
 
   cin >> r;
 
   cout << "Area : " << pi *r * r;
+  cout << "Round : " << pi *2 *r ;
+  cout << "Volume : " << pi *r *r *h;
   return 0;
 }
